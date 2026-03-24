b1ed55dc0a8510b09713299ee251c8ec081c192b
SA JAEHEON <jls15900@gmail.com>
2026-03-25 08:41:29 +0900

    리눅스 전용 스크립트 추가

---
+++ fuzz_script.sh
@@ -0,0 +1,5 @@
+JAZZER_FUZZ=1 mvn test -Dmaven.test.failure.ignore=true -Djazzer.keep_going=0 -Dtest=SnakeYamlFuzzTest
+
+# 확실하지 않음
+JAZZER_ARGS="--keep_going=0" JAZZER_FUZZ=1 mvn test -Dtest=SnakeYamlFuzzTest
+$env:JAZZER_ARGS="--keep_going=0"; $env:JAZZER_FUZZ="1"; mvn test -Dtest=SnakeYamlFuzzTest
