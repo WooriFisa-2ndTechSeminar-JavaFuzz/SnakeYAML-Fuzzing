5d56b90df58cb9ef29abe831ca08a76403168277
SA JAEHEON <jls15900@gmail.com>
2026-03-25 19:40:27 +0900

    퍼징을 위한 기본 명령어 정리

--- fuzz_script.sh
+++ fuzz_script.sh
@@ -1,3 +1,7 @@
+# maven install - skip tests
+mvn install -DskipTests
+
+# fuzzing(bash)
 JAZZER_FUZZ=1 mvn test -Dmaven.test.failure.ignore=true -Djazzer.keep_going=0 -Dtest=SnakeYamlFuzzTest
 
 # 확실하지 않음
