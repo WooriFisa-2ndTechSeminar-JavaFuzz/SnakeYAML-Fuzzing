c51c57c77637fb60c56712c4497e84f30e6dbecd
SA JAEHEON <jls15900@gmail.com>
2026-03-29 18:52:21 +0900

    퍼징, 회귀 테스트 명령어 확실하게 확인

--- fuzz_script.sh
+++ fuzz_script.sh
@@ -4,6 +4,5 @@ mvn install -DskipTests
 # fuzzing(bash)
 JAZZER_FUZZ=1 mvn test -Dmaven.test.failure.ignore=true -Djazzer.keep_going=0 -Dtest=SnakeYamlFuzzTest
 
-# 확실하지 않음
-JAZZER_ARGS="--keep_going=0" JAZZER_FUZZ=1 mvn test -Dtest=SnakeYamlFuzzTest
-$env:JAZZER_ARGS="--keep_going=0"; $env:JAZZER_FUZZ="1"; mvn test -Dtest=SnakeYamlFuzzTest
+# regression test
+mvn test -Dmaven.test.failure.ignore=true -Djazzer.keep_going=0 -Dtest=SnakeYamlFuzzTest
