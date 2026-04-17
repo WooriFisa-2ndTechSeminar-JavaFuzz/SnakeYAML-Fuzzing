655c423306e9adf71d59339256ccf8290d96094a
SA JAEHEON <113874212+Zaixian5@users.noreply.github.com>
2026-04-17 09:54:07 +0900

    회귀 테스트 명령 주석 처리

--- fuzz_script.sh
+++ fuzz_script.sh
@@ -1,8 +1,8 @@
 # maven install - skip tests
 mvn install -DskipTests
 
-# fuzzing(bash)
+# fuzzing
 JAZZER_FUZZ=1 mvn test -Dmaven.test.failure.ignore=true -Djazzer.keep_going=0 -Dtest=SnakeYamlFuzzTest
 
 # regression test
-mvn test -Dmaven.test.failure.ignore=true -Djazzer.keep_going=0 -Dtest=SnakeYamlFuzzTest
+# mvn test -Dmaven.test.failure.ignore=true -Djazzer.keep_going=0 -Dtest=SnakeYamlFuzzTest
